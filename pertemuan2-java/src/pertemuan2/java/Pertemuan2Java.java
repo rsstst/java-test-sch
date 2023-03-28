0626dc7d3f2a7a6aa6e19dcfbb7189a262f14144
C-5 <C-5@DESKTOP-45JQ2MN>
2023-03-28 08:22:07 +0700

    added proj, finished 1-7 assignment

---
+++ pertemuan2-java/src/pertemuan2/java/Pertemuan2Java.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan2.java;
+
+/**
+ *
+ * @author C-5
+ */
+public class Pertemuan2Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nilaiA = 10;
+        int nilaiB = 20;
+        
+        double pi = 3.14;
+        double eksponen = 2.71;
+        
+        boolean isPlus = true;
+        
+        System.out.println(nilaiA);
+        System.out.println(nilaiB);
+        System.out.println(pi);
+        System.out.println(eksponen);
+        System.out.println(isPlus);
+        
+    }
+    
+}
