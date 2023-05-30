dfb1356d7b8c78cfc824c9b269d55fa9328e1efa
rsstst <snepshot551@gmail.com>
2023-05-30 07:48:49 +0700

    added pertemuan8, finished latihan 1

---
+++ Pertemuan8/src/main/java/com/rest/pertemuan8/Pertemuan8.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rest.pertemuan8;
+
+/**
+ *
+ * @author C-8
+ */
+public class Pertemuan8 {
+    
+}
