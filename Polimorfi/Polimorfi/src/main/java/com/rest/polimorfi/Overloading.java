aec1c3112d6833a50fac0a5abf24b293aeff39f4
rsstst <snepshot551@gmail.com>
2023-07-04 07:45:08 +0700

    initial commit

---
+++ Polimorfi/Polimorfi/src/main/java/com/rest/polimorfi/Overloading.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rest.polimorfi;
+
+/**
+ *
+ * @author C-17
+ */
+
+class Overloading {
+    public static void main (String[] args){
+        System.out.println("Contoh demonstrasi metode overload");
+        Addition add = new Addition();
+        int sumInt = add.sum(10, 20);
+        System.out.println("10 + 20 = " + sumInt);
+        double sumDouble = add.sum(10.5, 11.5);
+        System.out.println("10.5 + 11.5 = " + sumDouble);
+        String sumString = add.sum("Oh", "yeah");
+        System.out.println("'Oh' + 'yeah' is " + sumString);
+    }
+}
---
+++ Polimorfi/Polimorfi/src/main/java/com/rest/polimorfi/add.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rest.polimorfi;
+
+/**
+ *
+ * @author C-17
+ */
+class Addition {
+            int sum(int x, int y){
+                return x + y;
+            }
+            
+            double sum(double x, double y){
+                return x + y;
+            }
+            
+            String sum(String s1, String s2){
+                return s1.concat(s2);
+            }
+    }
